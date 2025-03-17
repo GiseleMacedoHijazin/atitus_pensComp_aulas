b4f03b0bc7c9a06b80362bb2f4d94449be76b28b
Gisele Hijazin <giselehijazin@gmail.com>
2025-03-17 19:41:18 -0300

    Update media_idades.py

--- 01_conhecendo_o_python/media_idades.py
+++ 01_conhecendo_o_python/media_idades.py
@@ -1,5 +1,9 @@
-idade_A = 19
+idade_A = 30
 idade_B = 19
 idade_C = 21
-media = (idade_B + idade_A + idade_C) / 3
+idade_D = 25
+idade_E = 14
+
+media = (idade_B + idade_A + idade_C + idade_D + idade_E) / 5
+script =
 
