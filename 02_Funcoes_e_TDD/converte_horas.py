2034f0858d3ed048c0885a0be0dac91213d0e034
Gisele Hijazin <giselehijazin@gmail.com>
2025-03-19 19:34:27 -0300

    Update converte_horas.py

--- 02_Funcoes_e_TDD/converte_horas.py
+++ 02_Funcoes_e_TDD/converte_horas.py
@@ -19,12 +19,12 @@ def test():
     assert minuto_para_segundo(1) == 60
     assert minuto_para_segundo(2) == 120
  
-   assert hora_para_segundo(0) == 0
-   assert hora_para_segundo(1) == 3600  # 1 * 60 * 60
-   assert hora_para_segundo(2) == 7200  # 2 * 60 * 60
+    assert hora_para_segundo(0) == 0
+    assert hora_para_segundo(1) == 3600  # 1 * 60 * 60
+    assert hora_para_segundo(2) == 7200  # 2 * 60 * 60
 
-  assert dia_para_segundo(0)==0
-  assert dia_para_segundo(1)==86.400
-  assert dia_para_segundo(2)==172.800
+    assert dia_para_segundo(0)==0
+    assert dia_para_segundo(1)==86.400
+    assert dia_para_segundo(2)==172.800
 
 print("Terminou com sucesso!")
