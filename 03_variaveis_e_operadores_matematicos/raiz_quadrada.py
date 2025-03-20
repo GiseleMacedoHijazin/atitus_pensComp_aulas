8ce7431ddfcd5f3511a41acb7257d4f66a66277c
Gisele Hijazin <giselehijazin@gmail.com>
2025-03-19 21:53:10 -0300

    Update raiz_quadrada.py

--- 03_variaveis_e_operadores_matematicos/raiz_quadrada.py
+++ 03_variaveis_e_operadores_matematicos/raiz_quadrada.py
@@ -1,6 +1,10 @@
-def raiz_quadrada(valor) = x**1/2
+def raiz_quadrada(valor):
+   return valor**(1/2)
+
    
 def test():
     assert raiz_quadrada(9) == 3
     assert raiz_quadrada(16) == 4
     assert raiz_quadrada(25) == 5
+
+print(raiz_quadrada(9))
