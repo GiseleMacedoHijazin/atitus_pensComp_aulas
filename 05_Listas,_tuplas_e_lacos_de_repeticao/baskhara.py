4cdd0e0c280df5db433399a9f3bf6a5a2911281e
Gisele Hijazin <giselehijazin@gmail.com>
2025-04-14 16:07:18 -0300

    Update baskhara.py

--- 05_Listas,_tuplas_e_lacos_de_repeticao/baskhara.py
+++ 05_Listas,_tuplas_e_lacos_de_repeticao/baskhara.py
@@ -8,7 +8,7 @@ def baskhara(a, b, c):
     x2 = (-b -discriminante**(1/2)) / (2 * a)
     return [x1,x2]
 
-def test()
+def test():
     assert baskhara(1, -3, 2) == [2, 1]
     assert baskhara(2, 3, -2) == [-2, 0.5]
     assert baskhara(1, -5, 6) == [2, 3]
