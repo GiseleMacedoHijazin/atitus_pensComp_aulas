0052d66b83cacf68d781d74c071c21578f9c3f4b
Gisele Hijazin <giselehijazin@gmail.com>
2025-04-22 10:55:15 -0300

    Update extra_03.py

--- 07_Exercicios/extra_03.py
+++ 07_Exercicios/extra_03.py
@@ -1,7 +1,7 @@
 def real_para_dolar(valor, tx_conversao):
     tx_conversao = 5.20
-    valor = int(input("Informe um valor para converter")
-                print(f"O valor informado será convertido usando a taxa de {tx_conversao}: {valor} / {tx_conversao}")
+    valor = int(input("Informe um valor para converter"))
+    print(f"O valor informado será convertido usando a taxa de {tx_conversao}: {valor} / {tx_conversao}")
 
 def test():
     assert real_para_dolar(500, 5.20) == 96.23
