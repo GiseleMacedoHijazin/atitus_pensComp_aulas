5f5ef4c2419c1b267be5e7f8885a4c79386866ae
Gisele Hijazin <giselehijazin@gmail.com>
2025-05-20 20:01:54 -0300

    Update verifica_positivo.py

--- 04_operadores_logicos_e_condicoes/verifica_positivo.py
+++ 04_operadores_logicos_e_condicoes/verifica_positivo.py
@@ -2,9 +2,10 @@ def eh_positivo(numero):
     return numero > 0 
 
 def eh_negativo(numero):
-    return not eh_positivo(numero)
-    
+    return numero < 0 
+
 def test():
+    # Testes para a função eh_positivo
     assert eh_positivo(1)
     assert eh_positivo(2)
     assert eh_positivo(10)
@@ -12,6 +13,7 @@ def test():
     assert not eh_positivo(-1)
     assert not eh_positivo(-10)
 
+    # Testes para a função eh_negativo
     assert eh_negativo(-1)
     assert eh_negativo(-2)
     assert eh_negativo(-10)
