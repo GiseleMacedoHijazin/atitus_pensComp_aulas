db604c729338c77e74c89133aa587a57ac1d2718
Gisele Hijazin <giselehijazin@gmail.com>
2025-05-21 18:25:49 -0300

    Update print_e_input.py

--- 01_conhecendo_o_python/print_e_input.py
+++ 01_conhecendo_o_python/print_e_input.py
@@ -1,8 +1,6 @@
 def saudacao():
-    name = "Gisele"  
-    last_name = "Macedo" 
-    print("Olá,", name)
-    print("Olá,", last_name)
+    name = "Gisele"
+    last_name = "Macedo"
+    print("Olá,", name, last_name)
 
-# Executando a função
 saudacao()
