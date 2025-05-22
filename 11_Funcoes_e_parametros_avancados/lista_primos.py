e6ca0fc41178a9ebe45ab1c0b4abc50accc850ed
Gisele Hijazin <giselehijazin@gmail.com>
2025-05-21 23:36:44 -0300

    Update lista_primos.py

--- 11_Funcoes_e_parametros_avancados/lista_primos.py
+++ 11_Funcoes_e_parametros_avancados/lista_primos.py
@@ -1,19 +1,22 @@
-def eh_primo(numero: int) -> bool:
-    if num <= 1:
-       return False
-    for i in range(2, num)
-        if num % i == 0 
-           return False
-   return True
+def eh_primo(numero: int) -> bool: 
+    if numero <= 1:
+        return False
+    i = 2
+    while numero % i != 0:
+        i = i + 1
+    if i == numero:
+        return True
+    return False
 
-    
-def lista_primos(val: int) -> list:
-    primos = []
-    for x in range (val + 1):
-        if eh_primo(x):
-            primos.append(x)
-    return primos
+def lista_primos(numero: int) -> list:
+    lista_primos = []
+    for j in range(numero + 1): 
+        if eh_primo(j): 
+            lista_primos.append(j)
+    return lista_primos
 
-assert lista_primos(10) == [2, 3, 5, 7]
-assert lista_primos(13) == [2, 3, 5, 7, 11, 13]
-assert lista_primos(50) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+
+def test():
+    assert lista_primos(10) == [2, 3, 5, 7]
+    assert lista_primos(13) == [2, 3, 5, 7, 11, 13]
+    assert lista_primos(50) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
