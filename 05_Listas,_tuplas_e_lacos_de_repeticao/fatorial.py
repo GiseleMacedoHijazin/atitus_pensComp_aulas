4e0860ab8f34335874d2bdef889cb6bba7e1c666
Gisele Hijazin <giselehijazin@gmail.com>
2025-05-21 23:13:47 -0300

    Update fatorial.py

--- 05_Listas,_tuplas_e_lacos_de_repeticao/fatorial.py
+++ 05_Listas,_tuplas_e_lacos_de_repeticao/fatorial.py
@@ -1,13 +1,12 @@
 def fatorial(numero):
-	if numero < 0:
-		return None 
-	if numero == 0:
-		return 1
-	resultado = 1
-	for n in range(1, numero + 1):
-		resultado *= n
-		print(n, resultado)
-	return resultado
+    if numero < 0:
+        return None 
+    if numero == 0:
+        return 1
+    resultado = 1
+    for n in range(1, numero + 1):
+        resultado *= n
+    return resultado
 
 def test():
     assert fatorial(0) == 1
