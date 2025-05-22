3d2e6cb24f17b9c6feaa1bdc99d6a2f343120dce
Gisele Hijazin <giselehijazin@gmail.com>
2025-05-21 23:07:02 -0300

    Update baskhara.py

--- 05_Listas,_tuplas_e_lacos_de_repeticao/baskhara.py
+++ 05_Listas,_tuplas_e_lacos_de_repeticao/baskhara.py
@@ -1,21 +1,21 @@
+import math 
 def baskhara(a, b, c):
-    discriminante = (b**2) - ((4*a)*c)
+    discriminante = b**2 - 4*a*c
     if discriminante < 0:
         return None
-    if discriminante == 0:
-        return x1 or x2
-    x1 = (-b + discriminante**(1/2)) / (2 * a)
-    x2 = (-b -discriminante**(1/2)) / (2 * a)
-    return [x1,x2]
+    elif discriminante == 0:
+        x = -b / (2 * a)
+        return x
+    else:
+        x1 = (-b + math.sqrt(discriminante)) / (2 * a)
+        x2 = (-b - math.sqrt(discriminante)) / (2 * a)
+        return [x1, x2]
 
 def test():
     assert baskhara(1, -3, 2) == [2, 1]
-    assert baskhara(2, 3, -2) == [-2, 0.5]
-    assert baskhara(1, -5, 6) == [2, 3]
-    assert baskhara(1, -7, 10) == [2, 5]
+    assert baskhara(2, 3, -2) == [0.5, -2]
+    assert baskhara(1, -5, 6) == [3, 2]
+    assert baskhara(1, -7, 10) == [5, 2]
 
     assert baskhara(1, 2, 3) is None
     assert baskhara(1, 0, 0) == 0
-    
-print(baskhara(1,-3,2))
-
