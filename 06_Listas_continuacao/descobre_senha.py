fff45d00354d570ce220114b665f89ce791703c6
Gisele Hijazin <giselehijazin@gmail.com>
2025-05-21 23:21:06 -0300

    Update descobre_senha.py

--- 06_Listas_continuacao/descobre_senha.py
+++ 06_Listas_continuacao/descobre_senha.py
@@ -1,13 +1,19 @@
-def senha():
-  palpites = int(input("Digite um número de 0 a 10:"))
-  senha = 7
-  tentativas = 1
-  while palpites != senha:
-    print("Senha errada.Tente novamente.")
-    tentativas +=1
-    palpites = int(input("Digite um número de 0 a 10:"))
-  print("Senha Correta! Você acertou após", tentativas, "tentativas")
-senha()
+def descobre_senha(senha_secreta, total_tentativas): 
+    tentativas = 0
+    for tentativa in total_tentativas:
+        if tentativa < 1 or tentativa > 10:
+            continue
+        tentativas += 1
+        if tentativa == senha_secreta:
+            return tentativas  
+    return -1  
+
+def test_descobre_senha():
+    assert descobre_senha(6, [6]) == 1
+    assert descobre_senha(6, [0, 11, 3, 6]) == 2  
+    assert descobre_senha(6, [1, 2, 3, 4, 5]) == -1
+    assert descobre_senha(6, [1, 2, 3, 4, 5, 6]) == 6
+
   
     
                    
