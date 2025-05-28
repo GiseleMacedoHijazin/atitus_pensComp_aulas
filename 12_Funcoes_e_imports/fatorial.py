73d841a489251ee988f80d1358a5e0253fa5170d
Gisele Hijazin <giselehijazin@gmail.com>
2025-05-27 21:19:15 -0300

    Update fatorial.py

--- 12_Funcoes_e_imports/fatorial.py
+++ 12_Funcoes_e_imports/fatorial.py
@@ -9,7 +9,7 @@ def fatorial_non_rec(n):
     if n == 0:
         return 1
     resultado = 1
-    for n in range( 1, n + 1)
+    for n in range(1, n + 1)
         resultado *= n
     return resultado
         
